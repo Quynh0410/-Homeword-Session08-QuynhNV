e7e851fc2ea30c7188d0e8a28424f010552848dc
Quynh0410 <nvq041091@gmail.com>
2024-09-16 14:37:38 +0700

    Add files via upload

---
+++ [Homework] Sessoin 08/homework08.js
@@ -0,0 +1,62 @@
+// // Exercises 01
+// let hello = "Xin chào";
+// alert(hello);
+
+
+// //Exercises 02
+// let i = 10;
+// console.log(i)
+
+// let f = 20.5;
+// console.log(f);
+
+// let b = true;
+// console.log(b);
+
+// let s ="Hà Nội";
+// console.log(s);
+
+//Exercises 03
+// let width = Number(prompt("Chiều rộng"));
+// let height = Number(prompt("Chiều cao"));
+// let sum = height + width;
+// console.log(sum);
+
+
+//Exercises 04
+// let age = "Bạn đã đủ 18 tuổi chưa?";
+// confirm(age);
+
+
+//Exercises 05
+// let firstNumber = Number(prompt("Vật lý"));
+// let secondNumber = Number(prompt("Hóa học"));
+// let thirdNumber = Number(prompt("Sinh học"));
+// let sum = (firstNumber + secondNumber + thirdNumber) / 3;
+// console.log(sum);
+
+//Exercises 06
+// let celsius = Number(prompt("Nhiệt độ (°C)"))
+// let fahrenheit = (celsius * 9/5) + 32;
+// console.log(fahrenheit);
+
+//Exercises 07
+// let width = Number(prompt("Chiều rộng hình chữ nhật"));
+// let height = Number(prompt("Chiều cao hình chữ nhật"));
+// console.log("Chu vi hình chữ nhật", 2 * (width + height));
+// console.log("Diện tích hình chữ nhật", (width * height));
+
+
+// //Exercises 08
+// let radius = Number(prompt("Hãy nhập bán kính đường tròn"));
+// let perimeter = 2 * Math.PI * radius;   
+// let acreage = Math.PI * Math.pow(radius, 2);
+// console.log("Chu vi của hình tròn là", perimeter)
+// console.log("Diện tích của hình tròn là", acreage)
+
+
+// //Exercises 09
+let a = 5;
+let b= "5";
+console.log(a==b);  console.log(a===b);
+console.log(a!=b);  console.log(a!==b);
